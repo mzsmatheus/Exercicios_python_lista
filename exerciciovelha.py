8c5f393b78a7eaaffddf654c217f0995de1553ef
Matheus Z. e Souza <63678546+mzsmatheus@users.noreply.github.com>
2023-08-31 20:17:59 -0300

    commit1

---
+++ exerciciovelha.py
@@ -0,0 +1,30 @@
+#ex jogo da velha
+"""
+tabuleiro = {
+    (0,0) : "-",
+    (0,1) : "-",
+    (0,2) : "-",
+    (0,3) : "-",
+    (1,0) : "-",
+    (1,1) : "-",
+    (1,2) : "-",
+    (1,3) : "-",
+    (2,0) : "-",
+    (2,1) : "-",
+    (2,2) : "-",
+    (2,3) : "-",
+    (3,0) : "-",
+    (3,1) : "-",
+    (3,2) : "-",
+    (3,3) : "-"
+    }
+
+linha1 = tabuleiro.values()
+linha1
+print(linha1)
+
+
+def ver_linha(linha):
+    return all([x == "x" for x in linha]) or return all([x == "O" for x in linha])
+
+"""
